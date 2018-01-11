fe74044c3928bb8bd3c9dfcaba1f22297680ecc6
Julia Eis <jeis@uni-bremen.de>
2018-01-11 13:39:42 +0100

    from cluster

--- test_HEF/gdir_initializing3.py
+++ test_HEF/gdir_initializing3.py
@@ -5,7 +5,6 @@ from oggm.core.massbalance import LinearMassBalance, PastMassBalance, RandomMass
 from oggm.core.flowline import FluxBasedModel
 from functools import partial
 FlowlineModel = partial(FluxBasedModel, inplace=False)
-from bayes_opt import BayesianOptimization
 
 import os
 import salem
@@ -82,7 +81,7 @@ def objfunc(param,gdir,start_fls,i):
                 #abs(real_model.volume_m3-y_1900.volume_m3)**2
 
     except:
-        f=np.inf
+        f=1e10
     print(param, f)
     return f
 
@@ -106,24 +105,19 @@ def find_initial_state(gdir):
 
     plt.figure()
 
-    ax1 = plt.subplot(411)
+    ax1 = plt.subplot(211)
     ax1.set_title(gdir.rgi_id)
     plt.setp(ax1.get_xticklabels(), visible=False)
-    plt.plot(x, y_1850.fls[-1].surface_h, 'k:', label='solution')
-    plt.plot(x, y_1850.fls[-1].bed_h, 'k', label='bed')
-    plt.legend(loc='best')
 
-    ax2 = plt.subplot(412, sharex=ax1)
+    ax2 = plt.subplot(212, sharex=ax1)
     plt.setp(ax2.get_xticklabels(), visible=False)
-    ax2.plot(x, y_1900.fls[-1].surface_h, 'k:', label='solution')
-    ax2.plot(x, y_1900.fls[-1].bed_h, 'k', label='bed')
-
+    '''
     ax3 = plt.subplot(413,sharex=ax1)
     ax3.plot(x, np.zeros(len(x)), 'k--')
 
     ax4 = plt.subplot(414, sharex=ax1)
     ax4.plot(x, np.zeros(len(x)), 'k--')
-
+    '''
     growing_climate = LinearMassBalance(past_climate.get_ela(1850),3)
 
     growing_model = FluxBasedModel(fls, mb_model=growing_climate,
@@ -131,32 +125,36 @@ def find_initial_state(gdir):
     y_start = copy.deepcopy(growing_model)
 
 
-    for i in [0,0.2,0.4,0.6,0.8,1,5,10,15,20,25,30,35,40,45,50,100]:
+    for i in [0,0.5,1,5,10,20,30,40,50]:
         res = minimize(objfunc, [0],args=(gdir,y_1900.fls,i,), method='COBYLA',
                        tol=1e-04, options={'maxiter':500,'rhobeg':2})
+        try:
+            result_model_1850,result_model_1900 = run_model(res.x,gdir,y_1900.fls,i)
 
-        result_model_1850,result_model_1900 = run_model(res.x,gdir,y_1900.fls,i)
+            f = np.sum(abs(result_model_1900.fls[-1].surface_h-y_1900.fls[-1].surface_h) ** 2) + np.sum(abs(y_1900.fls[-1].widths - result_model_1900.fls[-1].widths) ** 2)
 
-        f = np.sum(abs(result_model_1900.fls[-1].surface_h-y_1900.fls[-1].surface_h) ** 2) + \
-            np.sum(abs(y_1900.fls[-1].widths - result_model_1900.fls[-1].widths) ** 2)
+            #dif_s = result_model_1900.fls[-1].surface_h-y_1900.fls[-1].surface_h
+            #dif_w = result_model_1900.fls[-1].widths-y_1900.fls[-1].widths
 
-        dif_s = result_model_1900.fls[-1].surface_h-y_1900.fls[-1].surface_h
-        dif_w = result_model_1900.fls[-1].widths-y_1900.fls[-1].widths
+            ax1.plot(x, result_model_1850.fls[-1].surface_h,alpha=0.5, label=str(f))
+            ax2.plot(x, result_model_1900.fls[-1].surface_h,alpha=0.5)
+            #ax3.plot(x, dif_s)
+            #ax4.plot(x, dif_w)
+        except:
+            pass
+    ax1.plot(x, y_1850.fls[-1].surface_h, 'k', label='solution')
+    ax1.plot(x, y_1850.fls[-1].bed_h, 'k', label='bed')
+    plt.legend(loc='best')
+    ax2.plot(x, y_1900.fls[-1].surface_h, 'k', label='solution')
+    ax2.plot(x, y_1900.fls[-1].bed_h, 'k', label='bed')
 
-        ax1.plot(x, result_model_1850.fls[-1].surface_h, label=str(f))
-        ax2.plot(x, result_model_1900.fls[-1].surface_h)
-        ax3.plot(x, dif_s)
-        ax4.plot(x, dif_w)
 
-
-    plot_dir = os.path.join(cfg.PATHS['working_dir'],'plots','sliding_2000')
-    if not os.path.exists(plot_dir):
-        os.makedirs(plot_dir)
-    plt.savefig(os.path.join(plot_dir,gdir.rgi_id+'.png'))
-    plt.show()
+    plt.savefig(os.path.join(plot_dir,gdir.rgi_id+'.png'),dpi = (200))
+    #plt.show()
     #return True
 
 if __name__ == '__main__':
+    global plot_dir
     start_time = time.time()
     cfg.initialize()
     cfg.PATHS['dem_file'] = get_demo_file('srtm_oetztal.tif')
@@ -167,7 +165,9 @@ if __name__ == '__main__':
     cfg.PARAMS['prcp_scaling_factor']
     cfg.PARAMS['run_mb_calibration'] = True
     cfg.PARAMS['optimize_inversion_params'] = True
-
+    plot_dir = os.path.join(cfg.PATHS['working_dir'],'plots','sliding_2000')
+    if not os.path.exists(plot_dir):
+        os.makedirs(plot_dir)
     plt.rcParams['figure.figsize'] = (8, 8)  # Default plot size
 
     rgi = get_demo_file('rgi_oetztal.shp')
